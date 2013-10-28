af2b3dc27a12d64e4a2437ed1e81b608ce377337
ConstantGardener <david.friman@gmail.com>
2013-10-28 14:15:28 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'I0CBblDAGVY2ZIYOOyEWTn50EDsxOUg5iHF4az9o2niS8gR1', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1686301108], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'BioBrain Status', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
